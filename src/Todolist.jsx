bb761ed0e6ff1543a5e37c54365846bbd2843fcf
Jhamtanidev <jhamtanidev2004@gmail.com>
2023-07-29 23:51:57 +0530

    first commit

---
+++ src/Todolist.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { TodoItem } from "./Todoitem"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
+}
